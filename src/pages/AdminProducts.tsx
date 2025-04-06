4b31116011db2ef8d5d87302889b8631654bddff
Daniel-derf <danielpardinho321@gmail.com>
2025-04-06 15:26:08 -0300

    feat: admin products page structure

--- src/pages/AdminProducts.tsx
+++ src/pages/AdminProducts.tsx
@@ -1,7 +1,69 @@
-import React from "react";
+import { useEffect, useState } from "react";
+
+type Product = {
+  id: number;
+  name: string;
+  price: number;
+  sex: string;
+  description: string;
+  ratingId: number;
+  availableSizeQtt: { [size: string]: number };
+  appliedDiscountPercentage: number;
+};
+
+function useProducts() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  async function fetchProducts() {
+    setIsLoading(true);
+
+    const response = await fetch("http://localhost:3000/products");
+
+    if (!response.ok) {
+      const errorBody = await response.json();
+      setError(errorBody);
+
+      return;
+    }
+
+    const products = await response.json();
+
+    setProducts(products);
+    setIsLoading(false);
+  }
+
+  return { products, error, isLoading };
+}
 
 const AdminProducts = () => {
-  return <div>AdminProducts</div>;
+  const { products, error, isLoading } = useProducts();
+
+  return (
+    <div>
+      <h1>Produtos</h1>
+
+      {isLoading && (
+        <>
+          <h2>Carregando...</h2>
+        </>
+      )}
+
+      {error && <>Erro!</>}
+
+      {products &&
+        products.map((p) => (
+          <p key={p.id}>
+            {p.name} - R${p.price}
+          </p>
+        ))}
+    </div>
+  );
 };
 
 export default AdminProducts;

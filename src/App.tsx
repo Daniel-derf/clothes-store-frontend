a4da274582c3f047e0762628e321aee7ef6b773a
Daniel-derf <danielpardinho321@gmail.com>
2025-04-06 10:17:18 -0300

    feat: admin page structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
+// css
 import "./App.css";
+
+// libs
 import { Routes, Route } from "react-router-dom";
+
+// pages
 import Home from "./pages/Home";
+import AdminHome from "./pages/AdminHome";
+import AdminClients from "./pages/AdminClients";
+import AdminProducts from "./pages/AdminProducts";
+import AdminRegisterProduct from "./pages/AdminRegisterProduct";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
+
+      {/* admin routes */}
+      <Route path="/admin" element={<AdminHome />}></Route>
+      <Route path="/admin/products" element={<AdminProducts />}></Route>
+      <Route path="/admin/register-product" element={<AdminRegisterProduct />}></Route>
+      <Route path="/admin/clients" element={<AdminClients />}></Route>
     </Routes>
   );
 }
+
+/*
+ideia: ter uma página /admin para cadastrar produtos
+*/
 
 export default App;
---
+++ src/pages/AdminHome.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const AdminHome = () => {
+  const navigate = useNavigate();
+
+  const goToProducts = () => {
+    navigate("products");
+  };
+
+  const goToRegisterProduct = () => {
+    navigate("register-product");
+  };
+
+  const goToClients = () => {
+    navigate("clients");
+  };
+
+  return (
+    <div>
+      <h1>Página Administrativa</h1>
+
+      <div>
+        <button onClick={goToProducts}>VER PRODUTOS</button>
+
+        <button onClick={goToRegisterProduct}>CADASTRAR PRODUTO</button>
+
+        <button onClick={goToClients}>VER CLIENTES</button>
+      </div>
+    </div>
+  );
+};
+
+export default AdminHome;
